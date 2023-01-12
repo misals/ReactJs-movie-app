4e968a2138360696c9e60e395be958e6ff0049fd
Suraj Misal <surajmisal18@gmail.com>
2023-01-12 15:19:23 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// function logger
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware code
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    //console.log("ACTION_TYPE = ", action.type);
+    next(action);
+  };
+
+const thunk =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    if (typeof action === "function") {
+      action(dispatch);
+      return;
+    }
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 console.log("store", store);
 console.log("BEFORE STATE", store.getState());
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 
 import {
   ADD_MOVIES,
+  ADD_MOVIE_TO_LIST,
   ADD_TO_FAVOURITES,
   REMOVE_FROM_FAVOURITES,
   SET_SHOW_FAVOURITES,
+  ADD_SEARCH_RESULT,
 } from "../actions";
 
 const initialMoviesState = {
@@ -46,7 +48,11 @@ export function movies(state = initialMoviesState, action) {
         ...state,
         showFavourites: action.val,
       };
-
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
     default:
       return state;
   }
@@ -54,10 +60,25 @@ export function movies(state = initialMoviesState, action) {
 
 const initialSearchState = {
   result: {},
+  showSearchResults: false,
 };
 
 export function search(state = initialSearchState, action) {
-  return state;
+  //ADD_SEARCH_RESULT;
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        showSearchResults: false,
+      };
+    default:
+      return state;
+  }
 }
 
 const initialRootState = {
